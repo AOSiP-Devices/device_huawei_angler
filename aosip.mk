e595795312db559252557a04745ff84eb760f738
srisurya95 <srisurya95@gmail.com>
2018-07-07 09:50:45 +0200

    Angler: Update fingerprint to OPM6.171019.030.E1/4805388

--- aosip.mk
+++ aosip.mk
@@ -33,6 +33,6 @@ DEVICE_MAINTAINERS="Sri Harsha(srisurya95)"
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    PRIVATE_BUILD_DESC="angler-user OPM6.171019.030.B1 4768815 release-keys"
+    PRIVATE_BUILD_DESC="angler-user OPM6.171019.030.E1 4805388 release-keys"
 
-BUILD_FINGERPRINT= google/angler/angler:8.1.0/OPM6.171019.030.B1/4768815:user/release-keys \
+BUILD_FINGERPRINT= google/angler/angler:8.1.0/OPM6.171019.030.E1/4805388:user/release-keys \
